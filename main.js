38f9ee8b3296be81b1f27168e1af7acaf779cbf8
Vladislav <vladfyklev@gmail.com>
2022-06-27 14:45:52 +0300

    правки

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import SwipeCarousel from "./js/swipeCarousel.js";
 
 const carousel = new SwipeCarousel({
-    containerId: '.mySlider',
-    sliderId: '.mySlide',
+    // containerId: '.mySlider',
+    // sliderId: '.mySlide',
     interval: 2000,
     isPlaying: true
 });
 carousel.init();
+
